01828977629e4685e5619ba0741842738e79411a
Giovani Girardi <giovanigg08@gmail.com>
2022-01-14 16:35:50 -0300

    feat: Aplicação componentezada

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,55 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+
+import { api } from "../services/api";
+
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+
+export interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
 }
+
+interface SidebarProps {
+  selectedGenreId: number;
+  onGenreSelect: (id: number) => void;
+}
+
+export const SideBar: React.FC<SidebarProps> = ({
+  selectedGenreId,
+  onGenreSelect,
+}) => {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    onGenreSelect(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
